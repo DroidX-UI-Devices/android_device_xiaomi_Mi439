261404b199f0f705a770fd1e175a13a0ec647398
janak <niraulajanak2019@gmail.com>
2023-02-12 04:17:07 +0000

    DroidX:Initial Bringup

--- droidx_Mi439.mk
+++ droidx_Mi439.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DroidXUI stuff.
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Kernel
 TARGET_KERNEL_VERSION := 4.9
@@ -27,12 +27,21 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := lineage_Mi439
+PRODUCT_NAME := droidx_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+
+
+#DroidXUI props
+IS_OFFICIAL := true
+DROIDX_OFFICIAL := true
+DROIDX_BUILD_TYPE := OFFICIAL
+
+DROIDX_GAPPS := true
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
